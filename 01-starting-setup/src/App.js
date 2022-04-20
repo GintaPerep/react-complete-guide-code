87e1486c7f16f5e6f8917e3284ff1fe7f6a44ab0
Ginta <ginta.aizstrauta@gmail.com>
2022-04-20 10:19:12 +0300

    funkcijas alternatīva

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
-import Expenses from "./components/Expenses";
+/*Alternatīva kā var uzrakstīt funkciju*/
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -24,6 +26,38 @@ function App() {
     },
   ];
 
+/*function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];*/
+
+  /*return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, { items: expenses })
+  );*/
+
+  /*var uzrakstīt divos veidos, kā augstāk uzrakstīts return React.createElemtnt vai šeit zemāk uzrakstītais*/
+ 
   return (
     <div>
       <h2>Let's get started!</h2>
